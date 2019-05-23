abbb73ae3a14c9a93477bcfe608f62598eabfdb5
林兴宇 <linxingyu@souche.com>
2019-05-23 19:17:19 +0800

    work here default

--- bin/work-here.js
+++ bin/work-here.js
@@ -2,11 +2,13 @@ const inquirer = require('inquirer')
 
 const workHere = (db) => {
 	const pwd = process.cwd()
+	const name = pwd.split('/').pop()
 	inquirer.prompt([
 		{
 			type: 'input',
 			name: 'workspace',
 			message: 'Name your new workspace:',
+			default: name
 		}
 	]).then((answer) => {
 		if(answer.workspace.length === 0) return workHere()
